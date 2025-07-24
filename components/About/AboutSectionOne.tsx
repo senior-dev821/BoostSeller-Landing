ed47f4a1de177f54e085b6bb8b95e141493608b8
Vong <arncy65@gmail.com>
2025-07-24 23:46:40 +0700

    Fix content loading error

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -64,17 +64,21 @@ const AboutSectionOne: React.FC = () => {
                 <div className="mx-[-12px] flex flex-wrap">
 									{/* Problems */}
 									<div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-										<h4 className="mb-4 text-xl font-semibold text-primary">{content[1]}</h4>
+										<h4 className="mb-4 text-xl font-semibold text-primary">
+											{content[0] || 'Problems'}
+										</h4>
 										{listItems1.map((item, index) => (
-											<List key={`problem-${index}`} text={item} /> 
+											<List key={`problem-${index}`} text={item} />
 										))}
 									</div>
 
 									{/* Solutions */}
 									<div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-										<h4 className="mb-4 text-xl font-semibold text-primary">{content[2]}</h4>
+										<h4 className="mb-4 text-xl font-semibold text-primary">
+											{content[1] || 'Solutions'}
+										</h4>
 										{listItems2.map((item, index) => (
-											<List key={`solution-${index}`} text={item} /> 
+											<List key={`solution-${index}`} text={item} />
 										))}
 									</div>
 								</div>
